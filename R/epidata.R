345b9d92c139afd9a526b0eb3ed7cc019eaa848a
Andre Kim <mak@Andres-MacBook-Pro.local>
2019-12-12 11:48:36 -0800

    stratified

--- R/epidata.R
+++ R/epidata.R
@@ -109,6 +109,58 @@ format_data_glm_nosex <- function(d, exposure, is_e_categorical, min_cell_size =
 
 
 
+#' format_data_glm_energy
+#'
+#' @description
+#' Same as format_data_glm, but adding energytot as part of the dataset
+#'
+#' using energytot_imp
+#'
+#' @param d Input data - 'master' set I create
+#' @param exposure Exposure variable for GxEScanR
+#' @param is_e_categorical Is exposure categorical T/F
+#' @param min_cell_size Minimum cell size for study removal - tabulation of outcome, exposure, study if categorical. if numeric, tabulation of outcome and study
+#'
+#' @return Cleaned dataset
+#' @export
+#'
+#' @examples format_data_glm_energy(figi_gwas, 'asp_ref', T, 0)
+format_data_glm_energy <- function(d, exposure, is_e_categorical, min_cell_size = 0) {
+
+  tmp <- d %>%
+    dplyr::filter(gxe == 1) %>%
+    dplyr::mutate(outcome = fct_drop(outcome)) %>%
+    dplyr::select(vcfid, outcome, exposure, age_ref_imp, sex, energytot_imp, study_gxe, paste0(rep("PC", 3), seq(1,3))) %>%
+    filter(complete.cases(.))
+
+  if (is_e_categorical == T) {
+    drops <- data.frame(table(tmp$outcome, tmp[, exposure], tmp$study_gxe)) %>%
+      filter(Freq <= min_cell_size)
+    tmp <- filter(tmp, !study_gxe %in% unique(drops$Var3)) %>%
+      dplyr::mutate(study_gxe = fct_drop(study_gxe),
+                    outcome = factor(outcome, labels = seq(from = 0, length(levels(outcome)) - 1)),
+                    sex = factor(sex, labels = seq(from = 0, length(levels(sex)) - 1)),
+                    {{exposure}} := factor(get(exposure), labels = seq(from = 0, length(levels(get(exposure))) - 1)))
+
+    # return(tmp)
+  }
+  else {
+    drops <- data.frame(table(tmp$outcome, tmp$study_gxe)) %>%
+      filter(Freq <= min_cell_size)
+    tmp <- filter(tmp, !study_gxe %in% unique(drops$Var3)) %>%
+      dplyr::mutate(study_gxe = fct_drop(study_gxe),
+                    outcome = factor(outcome, labels = seq(from = 0, length(levels(outcome)) - 1)),
+                    sex = factor(sex, labels = seq(from = 0, length(levels(sex)) - 1)))
+    # return(tmp)
+  }
+
+  return(tmp)
+
+}
+
+
+
+
 
 #' format_data_gxescan
 #'
